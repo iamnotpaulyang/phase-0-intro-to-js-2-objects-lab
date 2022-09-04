df5a7155b645673e9e3fc6f6152a094e1f5246bb
Paul Yang <paulyang00@gmail.com>
2022-09-04 17:50:09 -0400

    commit tree 740c3daa19f3955e38402490f4e3096c1be1c07f

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = { name: "Paul", streetAddress: "502 Central Park", };
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {...obj,
+        [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue (obj, key, value){
+    obj[key] = value;
+    return obj;
+
+};
+
+function deleteFromEmployeeByKey (obj, key, value) {
+    return {...obj, 
+        [key]: value,
+    };
+};
+
+// function destructivelyDeleteFromEmployeeByKey(obj, key, value){
+//     return {...obj,
+//         [key]: value,
+//     };
+// };
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) { 
+    delete employee[key]; 
+    return employee; 
+    };
